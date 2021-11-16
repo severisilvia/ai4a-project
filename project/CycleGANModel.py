7f440e4062ded968976b191884535335a4d46940
severisilvia <silviaseveri97@gmail.com>
2021-11-16 17:37:13 +0100

    cycle

--- project/CycleGANModel.py
+++ project/CycleGANModel.py
@@ -8,35 +8,6 @@ from project.StyleGAN3 import StyleGAN3_Generator, StyleGAN3_Discriminator
 from project.utils.image_pool import ImagePool
 
 
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
-
-        #IMPEMENTARE QUI IL GENERATORE DI STYLEGAN3
-        self.conv = nn.Sequential()
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-
-        #IMPLEMENTARE QUI IL DISCRIMINATORE DI STYLEGAN3
-        self.main = nn.Sequential( )
-
-    def forward(self, input):
-        return self.main(input)
-
-
-#queste due sono da sistemare
-def LSGAN_D(real, fake):
-  return (torch.mean((real - 1)**2) + torch.mean(fake**2))
-
-def LSGAN_G(fake):
-  return  torch.mean((fake - 1)**2)
-
 
 class CycleGANModel():
 
