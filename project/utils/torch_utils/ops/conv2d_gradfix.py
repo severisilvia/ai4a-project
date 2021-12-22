79d66d67f38f7a1eb999b0e6c92e34630dd22100
Eronion <brandon41238@gmail.com>
2021-12-22 10:38:18 +0100

    tried to resolve dimensionality issue in train_main.py

--- project/utils/torch_utils/ops/conv2d_gradfix.py
+++ project/utils/torch_utils/ops/conv2d_gradfix.py
@@ -11,8 +11,6 @@ arbitrarily high order gradients with zero performance penalty."""
 
 import contextlib
 import torch
-import torch.nn.functional
-import torch.backends.cudnn
 
 # pylint: disable=redefined-builtin
 # pylint: disable=arguments-differ
