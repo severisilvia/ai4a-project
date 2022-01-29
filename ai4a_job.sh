75e4595fe54a655513241dd25920ea5524e32e1f
Eronion <brandon41238@gmail.com>
2022-01-29 12:56:05 +0100

    added sh train job

--- ai4a_job.sh
+++ ai4a_job.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=m3
-#SBATCH --output=/homes/lbaraldi/m3_log
-#SBATCH --error=/homes/lbaraldi/m3_log
+#SBATCH --job-name=ai4a
+#SBATCH --output=/homes/sseveri/logs/ai4a_log_29_01
+#SBATCH --error=/homes/sseveri/logs/ai4a_log_29_01
 #SBATCH --ntasks=2
 #SBATCH --gpus-per-task=1
 #SBATCH --partition=students-prod
-#SBATCH --mail-user=lorenzo.baraldi@unimore.it
+#SBATCH --mail-user=229635@studenti.unimore.it
 #SBATCH --mail-type=ALL
 
 . /usr/local/anaconda3/etc/profile.d/conda.sh
-conda activate m3
+conda activate stylegan3
 
-cd /homes/lbaraldi/m3
+cd /homes/sseveri/ai4a
 
 export MASTER_ADDR="aimagelab-srv-00"
 export MASTER_PORT=`comm -23 <(seq 8000 9000 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1`
@@ -19,12 +19,12 @@ export WORLD_SIZE=2
 export LOCAL_RANK=0
 
 export RANK=0
-srun -N1 -n1 -w $MASTER_ADDR --gpus=1 --exclusive python -u train.py <args> &
+srun -N1 -n1 -w $MASTER_ADDR --gpus=1 --exclusive python -u train_distributed.py <args> &
 sleep 5
 
 for i in {1..1}; do
 	  export RANK=$i
-	  srun -N1 -n1 --gpus=1 --exclusive python -u train.py <args> &
+	  srun -N1 -n1 --gpus=1 --exclusive python -u train_distributed.py <args> &
 done
 wait
 
